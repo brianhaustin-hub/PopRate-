24ff3502d5692a1ace53c4f4ab992ad3680d87dd
BRIAN HAUSTIN <brianhaustin360@gmail.com>
2026-09-23 01:58:02 +0300

    AI: Continue the to-do list. (cut off by time limit)

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '@/components/shell/Layout';
+import { HomeScreen } from '@/components/screens/HomeScreen';
+
+export default function Home() {
+  return (
+    <Layout>
+      <HomeScreen />
+    </Layout>
+  );
+}
