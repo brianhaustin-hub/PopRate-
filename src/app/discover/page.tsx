2a21db97af520ebbdbdd4d883d7db601c62edb03
BRIAN HAUSTIN <brianhaustin360@gmail.com>
2026-09-23 01:58:03 +0300

    AI: Continue the to-do list. (cut off by time limit)

---
+++ src/app/discover/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '@/components/shell/Layout';
+import { DiscoverScreen } from '@/components/screens/DiscoverScreen';
+
+export default function Discover() {
+  return (
+    <Layout>
+      <DiscoverScreen />
+    </Layout>
+  );
+}
