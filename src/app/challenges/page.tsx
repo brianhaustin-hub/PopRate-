d25bbb009fda3e13b1c47f04d0405ec49c79faf8
BRIAN HAUSTIN <brianhaustin360@gmail.com>
2026-09-23 01:58:10 +0300

    AI: Continue the to-do list. (cut off by time limit)

---
+++ src/app/challenges/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '@/components/shell/Layout';
+import { ChallengesScreen } from '@/components/screens/ChallengesScreen';
+
+export default function Challenges() {
+  return (
+    <Layout>
+      <ChallengesScreen />
+    </Layout>
+  );
+}
