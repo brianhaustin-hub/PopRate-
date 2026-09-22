1f565f9b932ccab5a9ab0b00d1c1730ff9adec10
BRIAN HAUSTIN <brianhaustin360@gmail.com>
2026-09-23 01:58:04 +0300

    AI: Continue the to-do list. (cut off by time limit)

---
+++ src/app/create/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '@/components/shell/Layout';
+import { CreateScreen } from '@/components/screens/CreateScreen';
+
+export default function Create() {
+  return (
+    <Layout>
+      <CreateScreen />
+    </Layout>
+  );
+}
