c3df35d40fae09dc05787ba6c015fac09f8dfd2a
BRIAN HAUSTIN <brianhaustin360@gmail.com>
2026-09-23 01:58:08 +0300

    AI: Continue the to-do list. (cut off by time limit)

---
+++ src/app/battle/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '@/components/shell/Layout';
+import { BattleScreen } from '@/components/screens/BattleScreen';
+
+export default function Battle() {
+  return (
+    <Layout>
+      <BattleScreen />
+    </Layout>
+  );
+}
